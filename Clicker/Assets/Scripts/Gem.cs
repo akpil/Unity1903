3bdd8cdcda5bff40ff9498f68eebc44d1765ea5b
Akpil <jyk0802@naver.com>
2019-05-29 16:30:32 +0900

    clicker project setup

---
+++ Clicker/Assets/Scripts/Gem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+    [SerializeField]
+    private Sprite[] Sprites;
+    private int currentIndex;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        currentIndex = 0;
+    }
+
+    public void ShowProgress(float current, float max)
+    {
+        float progressRate =  current / max;
+        float progressBase = 1f / Sprites.Length;
+
+        if (progressRate >= 1)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if ((currentIndex + 1) * progressBase < progressRate && progressRate < 1)
+        {
+            currentIndex++;
+            renderer.sprite = Sprites[currentIndex];
+        }
+    }
+}
---
+++ Clicker/Assets/Scripts/Scenemanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scenemanager : MonoBehaviour
+{
+    [SerializeField]
+    private Button GameStartButton;
+    [SerializeField]
+    private GameObject touchToStartText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameStartButton.onClick.AddListener(()=> { ShiftScene(1); });
+        StartCoroutine(BlinkingText(.7f));
+    }
+
+    public void ShiftScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    private IEnumerator BlinkingText(float countdown)
+    {
+        WaitForSeconds timeGap = new WaitForSeconds(countdown);
+        while (true)
+        {
+            touchToStartText.SetActive(!touchToStartText.activeInHierarchy);
+            yield return timeGap;
+        }
+    }
+}
